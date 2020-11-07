1530f74c214e569313da8c78cb8b89eab885ad81
Julius <juliustfrost@gmail.com>
2020-11-07 17:33:11 -0500

    commit progress

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField] private GameObject target;
+    [SerializeField] private float followScale = 0.01f;
+    private Transform _cameraTransform;
+    private Transform _targetTransform;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (target == null) {
+            Debug.Log("target not set!");
+        }
+        _targetTransform = target.GetComponent<Transform>();
+        _cameraTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var position = _cameraTransform.position;
+        var difference = Vector3.Scale((_targetTransform.position - position), new Vector3(1, 1, 0));
+
+        position += difference * followScale;
+        _cameraTransform.position = position;
+    }
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    private bool _jump;
+    [SerializeField] private float jumpPower = 10;
+    [SerializeField] private float torquePower = 1f;
+    private Rigidbody _rigidbodyComponent;
+    private bool _isGrounded;
+    private float _horizontalInput;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+
+        _horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // called every physics update
+    private void FixedUpdate()
+    {
+        if (_jump)
+        {
+            if (_isGrounded)
+                _rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            _jump = false;
+        }
+
+        // Debug.Log(_horizontalInput);
+        // rigidbodyComponent.AddRelativeTorque(new Vector3(0, horizontalInput * torquePower, 0), ForceMode.Impulse);
+        var angularVelocity = _rigidbodyComponent.angularVelocity;
+        angularVelocity = new Vector3(
+            angularVelocity.x,
+            angularVelocity.y,
+            angularVelocity.z - _horizontalInput * torquePower
+        );
+        _rigidbodyComponent.angularVelocity = angularVelocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _isGrounded = false;
+    }
+}
