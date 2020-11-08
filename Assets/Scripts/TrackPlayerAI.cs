e4dcfdad986463d4d66b2d57d1bb34cfb40e2beb
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-11-07 23:42:27 -0500

    Enemy Capsule that tracks player

---
+++ Assets/Scripts/TrackPlayerAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayerAI : MonoBehaviour
+{
+    [SerializeField] private float torquePower = 1;
+    private GameObject _player;
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var target = _player.transform.position;
+        if (target.x < transform.position.x)
+        {
+            _rigidbody.AddTorque(0, 0, torquePower, ForceMode.VelocityChange);
+        }
+        else
+        {
+            _rigidbody.AddTorque(0, 0, -torquePower, ForceMode.VelocityChange);
+        }
+    }
+}
