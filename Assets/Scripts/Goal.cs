105c38e631a600aa0982a3daed2b65dcb899b66d
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-11-08 15:18:18 -0500

    Update Goal.cs

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,13 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] private int sceneBuildIndex;
     private void OnCollisionEnter(Collision other)
     {
         foreach (var contactPoint in other.contacts)
         {
             if (contactPoint.otherCollider.CompareTag("Player"))
             {
-                SceneManager.LoadScene("VictoryScreen");
+                SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
             }
         }
     }
