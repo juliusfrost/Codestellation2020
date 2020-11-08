1f7635b1b85cd2215ae7cb342e7eb8dd00493067
Julius <juliustfrost@gmail.com>
2020-11-07 22:06:50 -0500

    walking and sitting versions. player angular drag

--- Assets/Scripts/StompableEnemy.cs
+++ Assets/Scripts/StompableEnemy.cs
@@ -3,7 +3,7 @@
 public class StompableEnemy : MonoBehaviour
 {
     // if the velocity of the player is greater than this amount, then we say the player stomped on the enemy 
-    [SerializeField] private float stompThreshold = 0.1f;
+    [SerializeField] private float stompThreshold;
     private void OnCollisionEnter(Collision other)
     {
         foreach (var contactPoint in other.contacts)
