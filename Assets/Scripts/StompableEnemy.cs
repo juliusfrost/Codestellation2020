7a7480eac6516cbca0a4da0b2881dadc6c4e16d2
Julius <juliustfrost@gmail.com>
2020-11-07 21:57:32 -0500

    enemy code

---
+++ Assets/Scripts/StompableEnemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class StompableEnemy : MonoBehaviour
+{
+    // if the velocity of the player is greater than this amount, then we say the player stomped on the enemy 
+    [SerializeField] private float stompThreshold = 0.1f;
+    private void OnCollisionEnter(Collision other)
+    {
+        foreach (var contactPoint in other.contacts)
+        {
+            // continue only if otherCollider is from the player
+            if (contactPoint.otherCollider.gameObject.layer != 8) continue;
+            if (contactPoint.otherCollider.GetComponent<Rigidbody>().velocity.y < -stompThreshold)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                // TODO: make player lose a life and restart
+                Debug.Log("vertical velocity not fast enough to kill");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WalkAroundAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class WalkAroundAI : MonoBehaviour
+{
+    [SerializeField] private float walkSeconds = 5;
+    [SerializeField] private float torquePower = 1;
+    private float _lastTimeDirectionChanged;
+    private bool _walkLeft;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _lastTimeDirectionChanged = Time.realtimeSinceStartup;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Time.realtimeSinceStartup - _lastTimeDirectionChanged > walkSeconds)
+        {
+            _walkLeft = !_walkLeft;
+            _lastTimeDirectionChanged = Time.realtimeSinceStartup;
+        }
+
+        if (_walkLeft) _rigidbody.AddTorque(0, 0, -torquePower, ForceMode.VelocityChange);
+        else _rigidbody.AddTorque(0, 0, torquePower, ForceMode.VelocityChange);
+    }
+}
